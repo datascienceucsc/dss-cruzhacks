af93e0dcb9521a0ef10f517f160c76cda14359a3
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-18 15:04:56 -0800

    fix merge conflicts

---
+++ server/server/constants.py
@@ -0,0 +1,5 @@
+
+ip_address = "http://127.0.0.1"
+
+facebook_port = 8050
+
--- server/server/routes.py
+++ server/server/routes.py
@@ -1,15 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request, abort, Blueprint, current_app
-import requests
+from .constants import ip_address, facebook_port
+
 
 router = Blueprint('router', __name__)
-
-facebook_data_port = 8050
 
 
 @router.route("/", methods=["GET", "POST"])
 def home():
-    facebook_data_url = f"http://localhost:{facebook_data_port}"
-    facebook_graph = requests.get(facebook_data_url).text
-    return render_template('home.html', title="ADD TITLE HER", iframe=facebook_graph)
-
-
+    facebook_addr = f"{ip_address}:{facebook_port}"
+    return render_template('home.html', title="Transparency USA", iframesrc=facebook_addr)
