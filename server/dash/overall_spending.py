9257efaa87a7779977909b07bca681b9371a7458
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-19 18:59:10 +0000

    fix spacing and remove debug

--- server/dash/overall_spending.py
+++ server/dash/overall_spending.py
@@ -96,4 +96,4 @@ def update(input_val):
         return update_fig(input_val, df_new, 'Amount Spent on Advertising (USD)')
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    app.run_server(debug=False, host="0.0.0.0", port=8050)
