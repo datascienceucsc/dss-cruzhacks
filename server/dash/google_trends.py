6e2de04a7c4c69d70600b2b6f6dc771714491be3
Julian Lehrer <jmlehrer@ucsc.edu>
2020-01-18 20:52:08 -0800

    removed debug mode from google_trends.py

--- server/dash/google_trends.py
+++ server/dash/google_trends.py
@@ -49,5 +49,5 @@ def update(candidate):
     return(draw_fig(candidate))
 
 if __name__ == '__main__':
-    app.run_server(debug = True, host="0.0.0.0", port=8051)
+    app.run_server(debug = False, host="0.0.0.0", port=8051)
 
