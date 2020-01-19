970c0d9596d245e6e0b84de390fe55bafd1c21b4
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-19 09:19:43 +0000

    link external stylesheet to dashboard

--- server/dash/candidate_dashboard.py
+++ server/dash/candidate_dashboard.py
@@ -56,7 +56,8 @@ def draw_gtrends(candidate):
     fig = go.Figure(data = data, layout = layout)
     return fig
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__,
+                external_stylesheets=["http://34.94.120.23/static/graph.css"])
 
 app.layout = html.Div([
     html.H1(id = 'candidate_name' 
