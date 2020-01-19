d8d6c499c8293e7c7f4777b2dc000b4472c82058
Julian Lehrer <jmlehrer@ucsc.edu>
2020-01-18 22:04:02 -0800

    removed debug mode from choro

--- server/dash/overall_spending_chloro.py
+++ server/dash/overall_spending_chloro.py
@@ -94,4 +94,4 @@ def update(input_val):
         return update_fig(input_val, data, 'Per  Capita  USD')
 
 if __name__=='__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=8052)
+    app.run_server(debug=False, host="0.0.0.0", port=8052)
