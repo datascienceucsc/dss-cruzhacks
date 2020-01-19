ee3f3d012de27095c9fdafa1fbfd40770e5636bd
Anders Poirel <andersjopo@gmail.com>
2020-01-19 00:50:58 -0800

    dashboard half functional

--- server/dash/candidate_dashboard.py
+++ server/dash/candidate_dashboard.py
@@ -9,14 +9,36 @@ from dash.dependencies import Input, Output
 from config import data_dir
 
 # data for approval
+approvals = pd.read_csv(os.path.join(data_dir, 'approval_polls.csv'))
 
 # data for google trends
 gtrends = pd.read_csv(os.path.join(data_dir, 'google_trends.csv'))
+
+
+# data for google ads
+
 
 # candidate selection options
 candidates = pd.read_csv(os.path.join(data_dir, 'candidates.csv'), sep = ';')
 drop_down_options = [{'label' : person, 'value' : person} 
                      for person in candidates['CANDIDATE']]
+
+# plotting favorability polls
+def draw_approvals(candidate):
+    curr_approvals = approvals[approvals['politician'].isin([candidate])]
+    data = go.Scatter(
+        x = curr_approvals['start_date'], y = curr_approvals['tot_favorable'],
+        mode = 'markers', marker_color = '#BEF0F3',
+        marker_size = curr_approvals['tot_favorable']
+    )
+    layout = go.Layout(
+        yaxis = dict(title_text = 'Percentage', range = [0,100]),
+        xaxis = dict(showgrid = False),
+        title = "Proportion of Americans Favorable to {}".format(candidate),
+        template = 'plotly_dark'
+    )
+    fig = go.Figure(data = data, layout = layout)
+    return fig
 
 # plotting google search interest
 def draw_gtrends(candidate):
@@ -34,11 +56,11 @@ def draw_gtrends(candidate):
     fig = go.Figure(data = data, layout = layout)
     return fig
 
-
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1('{}'.format('CANDIDATE NAME')),
+    html.H1(id = 'candidate_name' 
+    ),
 
     dcc.Dropdown(
         id = 'candidate_drop',
@@ -48,11 +70,18 @@ app.layout = html.Div([
 
     html.H2('Social Media'),
 
-    html.P('Social media following of Joe Biden'),
+    html.H4('Follower counts'),
 
     html.H2('Approval'),
 
+    dcc.Graph(id = 'approvals',
+              figure = draw_approvals("Joe Biden"),
+              config  = {'displayModeBar' : False}),
+
     html.H2('Google Ads'),
+
+    dcc.Graph(id = 'gads',
+             config  = {'displayModeBar' : False}),
 
     html.H2('Google Trends'),
 
@@ -62,11 +91,25 @@ app.layout = html.Div([
 ])
 
 @app.callback(
+    Output('approvals', 'figure'),
+    [Input('candidate_drop', 'value')]
+)
+def update_approvals(candidate):
+    return(draw_approvals(candidate))
+
+@app.callback(
     Output('gtrends', 'figure'),
     [Input('candidate_drop', 'value')]
 )
-def update(candidate):
+def update_gtrends(candidate):
     return(draw_gtrends(candidate))
+
+@app.callback(
+    Output('candidate_name', 'children'),
+    [Input('candidate_drop', 'value')]
+)
+def update_title(candidate):
+    return candidate
 
 if __name__ == '__main__':
     app.run_server(debug = True, host="0.0.0.0", port=8051)
